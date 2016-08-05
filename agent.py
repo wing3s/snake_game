d3995096e9f8a7322ab8927c51fc6b8f0569ab85
Wen-Hao Lee <wing3s@gmail.com>
2016-08-04 22:54:16 -0700

    finish run_bot and agent

--- agent.py
+++ agent.py
@@ -1,21 +1,34 @@
+import os
 import time
+import itertools as it
 import numpy as np
+from random import sample as rsample
 
 
 def experience_replay(batch_size):
-    memory = []
+    """
+    Coroutine of experience replay
+    Send in an experience and yield random batch experiences
+    """
+    mem = []
     while True:
-        exp = yield memory
-        memory.append(exp)
+        exp = yield rsample(mem, batch_size) if batch_size <= len(mem) else None
+        mem.append(exp)
 
-        
+
 def save_image(folder='images'):
+    """
+    Coroutine of image saving
+    """
+    from matplotlib import pyplot as plt
+    from matplotlib import colors
+
     if folder not in os.listdir('.'):
         os.mkdir(folder)
     frame_cnt = it.count()
 
     # Nokia screen color style
-    cmap = colors.ListedColormap(['#98C302', '#353C6A', '#425A02'])
+    cmap = colors.ListedColormap(['#010101', '#F70403', '#14FF04'])
     bounds = [0, 0.25, 0.75, 1]
     norm = colors.BoundaryNorm(bounds, cmap.N)
 
@@ -31,24 +44,27 @@ def save_image(folder='images'):
             extent=(0, shape[1], 0, shape[0]))
         plt.grid(True)
         plt.axis('off')
-        plt.savefig('%s/frame%05i.png' % (folder, frame_cnt.next()))
+        plt.savefig('%s/frame%06i.png' % (folder, frame_cnt.next()))
 
-        
+
 class Agent:
-    def __init__(self, model, nb_frames, game, size):
+    def __init__(self, model, nb_frames, game, actions, size):
         self.model = model
         self.nb_frames = nb_frames
         self.game = game
         self.width, self.height = size
-    
-    def train(self, game, size, nb_epochs, batch_size, gamma, save_model):
+        self.actions = actions
+        self.nb_actions = len(self.actions)
+
+    def train(self, nb_epochs, batch_size, gamma, save_model):
+        epsilon = 1.  # Probability to explore, 0 ~ 1
         exp_replay = experience_replay(batch_size)
         exp_replay.next()
         for i in xrange(nb_epochs):
             start_time = time.time()
             game = self.game(width=self.width, height=self.height)
             screen, _ = game.next()
-            state = np.asarray([screen]*NB_FRAMES)
+            state = np.asarray([screen]*self.nb_frames)
 
             if epsilon > .1:
                 # fine tune epsilon
@@ -59,11 +75,12 @@ class Agent:
                 while True:
                     if np.random.random() > epsilon:
                         # use prediction
-                        act_idx = np.argmax(self.model.predict(state[np.newaxis]), axis=-1)[0]
+                        act_idx = np.argmax(
+                            self.model.predict(state[np.newaxis]), axis=-1)[0]
                     else:
                         # explore
-                        act_idx = np.random.randint(len(POSSIBLE_ACTIONS))
-                    action = POSSIBLE_ACTIONS[act_idx]
+                        act_idx = np.random.randint(self.nb_actions)
+                    action = self.actions[act_idx]
 
                     screen, reward = game.send(action)
                     state_prime = np.roll(state, 1, axis=0)
@@ -77,26 +94,33 @@ class Agent:
                         targets = []
                         for s, a, r, s_prime in batch:
                             q_vals = self.model.predict(s[np.newaxis]).flatten()
-                            a_idx = POSSIBLE_ACTIONS.index(a)
+                            a_idx = self.actions.index(a)
                             if r < 0:
                                 q_vals[a_idx] = r
                             else:
-                                q_vals[a_idx] = r + gamma * self.model.predict(s_prime[np.newaxis]).max(axis=-1)
+                                optimal_future_val = self.model.predict(
+                                    s_prime[np.newaxis]).max(axis=-1)
+                                q_vals[a_idx] = r + gamma * optimal_future_val
                             inputs.append(s)
                             targets.append(q_vals)
-                        loss += self.model.train_on_batch(np.array(inputs), np.array(targets))
+                        loss += self.model.train_on_batch(
+                            np.array(inputs), np.array(targets))
             except StopIteration:
                 pass
             time_spent = time.time() - start_time
 
             if (i+1) % 10 == 0:
-                print 'Epoch %6i/%i, loss: %.6f, epsilon: %.3f [%is]' % (i+1, nb_epochs, loss, epsilon, int(time_spent))
- 
+                print 'Epoch %6i/%i, loss: %.6f, epsilon: %.3f [%is]' % (
+                    i+1, nb_epochs, loss, epsilon, int(time_spent))
+
         if save_model:
+            folder = 'models'
             if folder not in os.listdir('.'):
                 os.mkdir(folder)
-            model_name = 'model_%iw_%ih_%iepochs_%ibatch_%.2fgamma' % (self.width, self.height, nb_epochs, batch_size, gamma)
-            self.model.save_weights('%s/%s.h5' % (folder, model_name), overwrite=True)
+            model_name = 'model_%iw_%ih_%iepochs_%ibatch_%.2fgamma' % (
+                self.width, self.height, nb_epochs, batch_size, gamma)
+            self.model.save_weights(
+                '%s/%s.h5' % (folder, model_name), overwrite=True)
 
     def play(self):
         img_saver = save_image()
@@ -109,14 +133,16 @@ class Agent:
             img_saver.send(screen)
             frame_cnt = it.count()
             try:
-                state = np.asarray([screen] * NB_FRAMES)
+                state = np.asarray([screen] * self.nb_frames)
                 while True:
                     frame_cnt.next()
-                    act_idx = np.argmax(self.model.predict(state[np.newaxis]), axis=-1)[0]
-                    screen, _ = game.send(POSSIBLE_ACTIONS[act_idx])
+                    act_idx = np.argmax(
+                        self.model.predict(state[np.newaxis]), axis=-1)[0]
+                    screen, _ = game.send(self.actions[act_idx])
                     state = np.roll(state, 1, axis=0)
                     state[0] = screen
                     img_saver.send(screen)
             except StopIteration:
-                print 'Saved %4i frames for game %3i' % (frame_cnt.next(), game_cnt.next())
+                print 'Saved %4i frames for game %3i' % (
+                    frame_cnt.next(), game_cnt.next())
         img_saver.close()
--- run_bot.py
+++ run_bot.py
@@ -1,5 +1,4 @@
 from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Convolution2D
 from keras.layers.core import Dense, Flatten
 from keras.optimizers import RMSprop
@@ -12,19 +11,26 @@ def main():
     game_width = 12
     game_height = 9
     nb_frames = 4
+    actions = ((-1, 0), (1, 0), (0, -1), (0, 1), (0, 0))
 
     # Recipe of deep reinforcement learning model
     model = Sequential()
-    model.add(Convolution2D(16, nb_row=3, nb_col=3, activation='relu', input_shape=(nb_frames, game_height, game_width)))
+    model.add(Convolution2D(
+        16,
+        nb_row=3,
+        nb_col=3,
+        activation='relu',
+        input_shape=(nb_frames, game_height, game_width)))
     model.add(Convolution2D(32, nb_row=3, nb_col=3, activation='relu'))
     model.add(Flatten())
     model.add(Dense(256, activation='relu'))
-    model.add(Dense(len(POSSIBLE_ACTIONS)))
+    model.add(Dense(len(actions)))
     model.compile(RMSprop(), 'MSE')
-    
-    agent = Agent(model=model, nb_frames=nb_frames, game=snake_game, size=(game_width, game_height))
-    agent.train(nb_epochs=1000, batch_size=64, gamma=0.8, save_model=True)
-    agent.play(record=True)
+
+    agent = Agent(
+        model, nb_frames, snake_game, actions, size=(game_width, game_height))
+    agent.train(nb_epochs=10, batch_size=64, gamma=0.8, save_model=True)
+    agent.play()
 
 
 if __name__ == '__main__':
